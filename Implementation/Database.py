05dc6c2f67c05dcf6e92052acde43a6e03eb16fd
Vishal Pujeri <vishal.pujeri@intimetec.com>
2024-06-13 09:03:50 +0530

    Made some changes

--- Implementation/Database.py
+++ Implementation/Database.py
@@ -4,8 +4,8 @@ from datetime import datetime
 def connect_to_db():
     conn = mysql.connector.connect(
         host="127.0.0.1",
-        user="root",  # Replace with your MySQL username
-        password="Vishal@2515",  # Replace with your MySQL password
+        user="root",
+        password="Vishal@2515",
         database="cafetaria", auth_plugin='mysql_native_password'
     )
     return conn
